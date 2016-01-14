b6ad24748e7125e287f571b86d7f721c695cbe8e
Pavel Kovalenko <paul-kv@yandex.ru>
2016-01-15 01:52:30 +0300

    Remove outdated comment.

--- src/dxerr.h
+++ src/dxerr.h
@@ -11,8 +11,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //--------------------------------------------------------------------------------------
 
-// This version only supports UNICODE.
-
 #pragma once
 
 #if !defined(NOMINMAX)
